d46850ace5d79642f2d9ca11d6ae54fa2543877a
diannenicholeebarvia <dianneebarvi@gmail.com>
2017-11-09 12:45:57 +0800

    Initial commit

---
+++ app/src/main/java/com/ebarvia/externalstorage4itb/ActivityTwo.java
@@ -0,0 +1,21 @@
+package com.ebarvia.externalstorage4itb;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class ActivityTwo extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+    }
+
+
+
+}
+
